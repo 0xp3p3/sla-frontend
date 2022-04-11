ae207c40d1782335352a6b447e8fe750e55844b1
smartdev-125 <tiandage719@outlook.com>
2022-04-11 20:35:35 +0100

    remove Link from footer"

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { FC } from "react";
 
 const Footer: FC = () => {
@@ -6,20 +5,18 @@ const Footer: FC = () => {
     <div className="footer"><img src="images/pattern-big.png" loading="lazy" sizes="(max-width: 991px) 99vw, 100vw" srcSet="images/pattern-big-p-500.png 500w, images/pattern-big.png 1866w" alt="" className="absolute-bg" />
       <div className="container-l relative w-container">
         <div className="hor-cen center mob-vert">
-          <Link href="/home">
-            <a className="footer-logo w-inline-block"><img src="images/Group-41.png" loading="lazy" sizes="(max-width: 479px) 86vw, 250px" srcSet="images/Group-41-p-500.png 500w, images/Group-41.png 516w" alt="" className="footer-logo-img" /></a>
-          </Link>
+          <a href="/home" className="footer-logo w-inline-block"><img src="images/Group-41.png" loading="lazy" sizes="(max-width: 479px) 86vw, 250px" srcSet="images/Group-41-p-500.png 500w, images/Group-41.png 516w" alt="" className="footer-logo-img" /></a>
           <div className="absolute-div">
             <div className="footer-links">
               <div className="vert-left foot-div">
-                <Link href="/home#mint-llama"><a className="tb-15 foot-link">MINT LLAMA</a></Link>
-                <Link href="/home#about"><a className="tb-15 foot-link">ABOUT US</a></Link>
-                <Link href="/home#features"><a className="tb-15 foot-link">FEATURES</a></Link>
+                <a href="/home#mint-llama" className="tb-15 foot-link">MINT LLAMA</a>
+                <a href="/home#about" className="tb-15 foot-link">ABOUT US</a>
+                <a href="/home#features" className="tb-15 foot-link">FEATURES</a>
               </div>
               <div className="vert-left">
-                <Link href="/home#roadmap"><a className="tb-15 foot-link">ROADMAP</a></Link>
+                <a href="/home#roadmap" className="tb-15 foot-link">ROADMAP</a>
                 <a href="/documents/Whitepaper.pdf" target="_blank" rel="noreferrer" className="tb-15 foot-link">WHITE PAPER</a>
-                <Link href="/terms"><a aria-current="page" className="tb-15 foot-link _4 w--current">TERMS &amp; CONDITIONS</a></Link>
+                <a href="/terms" aria-current="page" className="tb-15 foot-link _4 w--current">TERMS &amp; CONDITIONS</a>
               </div>
             </div>
             <div className="foot-socials">
