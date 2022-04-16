7cfc5fdb38d390ed699bcc2742e8c2eb5f929a55
smartdev-125 <tiandage719@outlook.com>
2022-04-16 16:42:03 +0100

    update supply of traits

--- pages/disguiseRoom.tsx
+++ pages/disguiseRoom.tsx
@@ -25,7 +25,7 @@ const DisguiseRoom: NextPage = () => {
             <h1 className="h2">How it works</h1>
             <div className="vert-left stretch">
               <h2 className="h3 h-white">STEP 1: MINT YOUR TRAITS</h2>
-              <p className="p1 p-white">Every trait will cost you 2 $HAY. Select your Llama Agent from the dropdown list below, then you can start the minting process. </p>
+              <p className="p1 p-white">There are 5 trait categories: Eyes, Mouth, Skin, Clothing and Hats. Each category is its own collection and will generate a random trait when you mint from it.<br/><br/> Combining a trait with your Llama Agent is permanent, making all collections deflationary.<br/><br/> Each trait costs 2 $HAY. Before minting one, you must select your Llama Agent from the dropdown menu. </p>
             </div>
             <div className="w-layout-grid grid-2">
               <div data-hover="false" data-delay={0} id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d815dd-faba1442" data-w-id="c51ac5e2-ed2e-9254-8fc8-bd71e7d815dd" className="dropdown-2 w-dropdown">
@@ -38,32 +38,39 @@ const DisguiseRoom: NextPage = () => {
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d815f4-faba1442" />
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d815f5-faba1442" className="none-mob" />
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d815f6-faba1442" className="vert-left space-btw h-472">
-                <div className="vert-left">
-                  <div className="img-bg"><img src="images/Skin-15---Tiger.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Skin-15---Tiger-p-500.png 500w, images/Skin-15---Tiger-p-800.png 800w, images/Skin-15---Tiger.png 1000w" alt="" className="llama-img select dis" /></div>
+                <div className="vert-left marg-mob">
+                  <div className="img-bg" style={{marginBottom:'30px'}}>
+                    <img src="images/Skin-15---Tiger.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Skin-15---Tiger-p-500.png 500w, images/Skin-15---Tiger-p-800.png 800w, images/Skin-15---Tiger.png 1000w" alt="" className="llama-img select dis" />
+                  </div>
+                  <h3 className="h3 h-white mrg-d-34">Supply: 6,000</h3>
                 </div>
                 <a href="#" className="button trait-mint w-button">mint skin</a>
               </div>
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d81601-faba1442" className="vert-left space-btw h-472">
                 <div className="vert-left">
-                  <div className="img-bg"><img src="images/Eyes-34---Green-Frame.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Eyes-34---Green-Frame-p-500.png 500w, images/Eyes-34---Green-Frame-p-800.png 800w, images/Eyes-34---Green-Frame.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <div className="img-bg" style={{marginBottom:'30px'}}><img src="images/Eyes-34---Green-Frame.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Eyes-34---Green-Frame-p-500.png 500w, images/Eyes-34---Green-Frame-p-800.png 800w, images/Eyes-34---Green-Frame.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <h3 className="h3 h-white mrg-d-34">Supply: 5,000</h3>
                 </div>
                 <a href="#" className="button trait-mint w-button">mint eyes</a>
               </div>
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d8160e-faba1442" className="vert-left space-btw h-472">
                 <div className="vert-left">
-                  <div className="img-bg"><img src="images/Mouth-06---Smoking-Marijuana.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Mouth-06---Smoking-Marijuana-p-500.png 500w, images/Mouth-06---Smoking-Marijuana-p-800.png 800w, images/Mouth-06---Smoking-Marijuana.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <div className="img-bg" style={{marginBottom:'30px'}}><img src="images/Mouth-06---Smoking-Marijuana.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Mouth-06---Smoking-Marijuana-p-500.png 500w, images/Mouth-06---Smoking-Marijuana-p-800.png 800w, images/Mouth-06---Smoking-Marijuana.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <h3 className="h3 h-white mrg-d-34">Supply: 6,000</h3>
                 </div>
                 <a href="#" className="button trait-mint w-button">mint mouth</a>
               </div>
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d8161b-faba1442" className="vert-left space-btw h-472">
                 <div className="vert-left">
-                  <div className="img-bg"><img src="images/Clothing-13---Cowboy.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Clothing-13---Cowboy-p-500.png 500w, images/Clothing-13---Cowboy-p-800.png 800w, images/Clothing-13---Cowboy.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <div className="img-bg" style={{marginBottom:'30px'}}><img src="images/Clothing-13---Cowboy.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Clothing-13---Cowboy-p-500.png 500w, images/Clothing-13---Cowboy-p-800.png 800w, images/Clothing-13---Cowboy.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <h3 className="h3 h-white mrg-d-34">Supply: 5,000</h3>
                 </div>
                 <a href="#" className="button trait-mint w-button">mint clothing</a>
               </div>
               <div id="w-node-c51ac5e2-ed2e-9254-8fc8-bd71e7d81628-faba1442" className="vert-left space-btw h-472">
                 <div className="vert-left">
-                  <div className="img-bg"><img src="images/Hat-05---Crown.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Hat-05---Crown-p-500.png 500w, images/Hat-05---Crown-p-800.png 800w, images/Hat-05---Crown.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <div className="img-bg" style={{marginBottom:'30px'}}><img src="images/Hat-05---Crown.png" loading="lazy" sizes="(max-width: 991px) 200px, 330px" srcSet="images/Hat-05---Crown-p-500.png 500w, images/Hat-05---Crown-p-800.png 800w, images/Hat-05---Crown.png 1000w" alt="" className="llama-img select dis" /></div>
+                  <h3 className="h3 h-white mrg-d-34">Supply: 5,000</h3>
                 </div>
                 <a href="#" className="button trait-mint w-button">mint hat</a>
               </div>
