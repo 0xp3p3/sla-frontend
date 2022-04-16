d2435b29967a44cb9aba01367dba8f8e2cb03e87
smartdev-125 <tiandage719@outlook.com>
2022-04-16 20:23:31 +0100

    update envs

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,9 +6,12 @@ import MintFromCandyMachine from "../components/solana/MintFromCandyMachine";
 
 const Index: NextPage = () => {
 
+  const cm_id = process.env.NEXT_PUBLIC_CM_ID_AVATAR!
+  console.log('cm_id: ', cm_id)
+
   return (
     <MintFromCandyMachine 
-      candyMachinePubkey={new PublicKey(process.env.NEXT_PUBLIC_CM_ID_AVATAR!)}
+      candyMachinePubkey={new PublicKey(cm_id)}
       collectionMint={new PublicKey(process.env.NEXT_PUBLIC_COLLECTION_AVATAR!)}
       rpcUrl={process.env.NEXT_PUBLIC_SOLANA_ENDPOINT!} 
     />
