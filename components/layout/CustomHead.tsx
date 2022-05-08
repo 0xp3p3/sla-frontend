e311bcc29e772539633070df97a36f8a5128d804
smartdev-125 <tiandage719@outlook.com>
2022-05-08 11:24:42 +0100

    add more meta tags

--- components/layout/CustomHead.tsx
+++ components/layout/CustomHead.tsx
@@ -8,19 +8,26 @@ interface Props {
 
 const CustomHead = (props: Props) => {
   const url = "https://www.secretllamaagency.com"
+  const description = "The Secret Llama Agency is an ecosystem running on the Solana blockchain, featuring the first ever PFP project with customizable traits and aliases. There are 25 Alpaca imposters hidden within the collection. Protect the Agency from sneaky Alpacas and their evil plans!"
+  const image = url + props.ogImageSource
+
 
   return (
     <Head>
+        <title>{props.title}</title>
+
+        <meta name="description" content={description} />
+
         <meta property="og:url" content={url} />
         <meta property="og:type" content="website" key="type" />
         <meta property="og:title" content={props.title}  key="title"/>
-        <meta property="og:description" content="The Secret Llama Agency is an ecosystem running on the Solana blockchain, featuring the first ever PFP project with customizable traits and aliases. There are 25 Alpaca imposters hidden within the collection. Protect the Agency from sneaky Alpacas and their evil plans!" key="description" />
-        <meta property="og:image" content={url + props.ogImageSource} key="image" />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} key="image" />
 
-        <meta property="twitter:title" content={props.title} key="twitter_title" />
+        <meta property="twitter:title" content="The Secret Llama Agency" key="twitter_title" />
         <meta property="twitter:card" content="summary" />
-        
-        <title>{props.title}</title>
+        <meta property="twitter:site" content="@SecretLlama_A" />
+        <meta property="twitter:image" content={image} />
     </Head>
   )
 }
