4c6cb790582fe1f679572dc2752bcbb66a901546
smartdev-125 <tiandage719@outlook.com>
2022-06-07 22:53:44 +0100

    remove hardcoded go-live date

---
+++ hooks/useCountdown.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+
+
+const extractEnvVar = (value: string | null): string | null => {
+  return (value && (value !== 'null')) ? value! : null
+} 
+
+
+const useCountdown = () => {
+  const goLiveDate = extractEnvVar(process.env.NEXT_PUBLIC_GO_LIVE_DATE) ? new Date(process.env.NEXT_PUBLIC_GO_LIVE_DATE!) : null
+
+  const countDownDate = goLiveDate ? new Date(goLiveDate).getTime() : null
+
+  const [countDown, setCountDown] = useState<number | null>(
+    countDownDate ? countDownDate - new Date().getTime() : null
+  )
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate ? countDownDate - new Date().getTime() : null)
+    }, 1000)
+
+    return () => clearInterval(interval);
+  }, [countDownDate])
+
+  const isLive = () => {
+    return goLiveDate && countDown <= 0
+  }
+
+  return { goLiveDate, countDown, isLive }
+}
+
+export default useCountdown
