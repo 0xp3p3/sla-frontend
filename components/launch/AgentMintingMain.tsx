3b7b061b2291e6a4f92e13fb323d93cdddc55b68
smartdev-125 <tiandage719@outlook.com>
2022-06-27 23:08:35 +0200

    update end public mint counter

--- components/launch/AgentMintingMain.tsx
+++ components/launch/AgentMintingMain.tsx
@@ -32,7 +32,7 @@ const getCountdownTimes = (now: Date, target: Date): { times: TimeCountdown, pas
 const CountdownDisplay = ({ times }: { times: TimeCountdown }) => {
   const pad = (n) => n < 10 ? `0${n}` : n;
 
-  return <>{`${pad(times.days)}:${pad(times.hours)}:${pad(times.minutes)}:${pad(times.seconds)}`}</>
+  return <>{`${pad(times.hours)}:${pad(times.minutes)}:${pad(times.seconds)}`}</>
 }
 
 const Countdown = ({ start, end, now, noTimeDisplay }: { start: Date, end: Date, now: Date, noTimeDisplay?: boolean }) => {
@@ -130,7 +130,7 @@ const AgentMintingMain = () => {
                 <Menu vertical style={{ margin: "auto" }}>
                   <Menu.Item className="header">Public Mint</Menu.Item>
                   <Menu.Item>
-                    <Countdown start={publicSaleStart} end={publicSaleEnd} now={currentTime} noTimeDisplay />
+                    <Countdown start={publicSaleStart} end={publicSaleEnd} now={currentTime} />
                   </Menu.Item>
                 </Menu>
               </Grid.Column>
