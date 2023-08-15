0043d1d802db81ed0eedfa995ce58b2c46b2819f
smartdev-125 <tiandage719@outlook.com>
2023-08-15 05:37:35 -0400

    update next config for canvas building

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ const withTM = require('next-transpile-modules')(['@blocto/sdk']);
 
 
 // For building canvas properly on vercel: https://github.com/Automattic/node-canvas/issues/1779
-// if (
-//   process.env.LD_LIBRARY_PATH == null ||
-//   !process.env.LD_LIBRARY_PATH.includes(
-//     `${process.env.PWD}/node_modules/canvas/build/Release:`,
-//   )
-// ) {
-//   process.env.LD_LIBRARY_PATH = `${
-//     process.env.PWD
-//   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-// }
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 
 /** @type {import('next').NextConfig} */
 module.exports = withTM({
