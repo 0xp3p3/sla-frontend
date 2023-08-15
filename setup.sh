3852666271bf1ad3d5fcaea249ee4532be68c876
smartdev-125 <tiandage719@outlook.com>
2023-08-15 06:19:32 -0400

    add patchelf command

--- setup.sh
+++ setup.sh
@@ -8,5 +8,14 @@ make
 cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
 cd ..
 
-# patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
-# patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+wget https://github.com/NixOS/patchelf/archive/refs/tags/0.18.0.tar.gz
+tar xf 0.18.0.tar.gz
+cd patchelf-0.18.0
+./bootstrap.sh
+./configure --prefix="$HOME/.local"
+make install
+strip --strip-unneeded ~/.local/bin/patchelf
+gzip -9 ~/.local/share/man/man1/patchelf.1
+
+patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
