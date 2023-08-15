2e543019b2d4ebfcdb414ef28581589609af478f
smartdev-125 <tiandage719@outlook.com>
2023-08-15 05:48:04 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+yum install wget
+
+wget https://zlib.net/fossils/zlib-1.2.9.tar.gz
+tar -xf zlib-1.2.9.tar.gz
+cd zlib-1.2.9
+sh configure
+make
+cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
+cd ..
+
+patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
