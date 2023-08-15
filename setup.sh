b2e71716afeeaf920db2484697100986fdbb9618
smartdev-125 <tiandage719@outlook.com>
2023-08-15 05:56:20 -0400

    comment patchelf in setup

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ make
 cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
 cd ..
 
-patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
-patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+# patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+# patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
