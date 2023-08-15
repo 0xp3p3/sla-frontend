c3fbefff2fc1d35453e0dcb763105f6bb9990a99
smartdev-125 <tiandage719@outlook.com>
2023-08-15 06:28:45 -0400

    retry with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 yum install wget
 
+wget https://github.com/NixOS/patchelf/archive/refs/tags/0.17.0.tar.gz
+tar -xf 0.17.0.tar.gz
+cd patchelf-0.17.0
+./bootstrap.sh
+./configure
+make
+make install
+cd ..
+
 wget https://zlib.net/fossils/zlib-1.2.9.tar.gz
 tar -xf zlib-1.2.9.tar.gz
 cd zlib-1.2.9
@@ -7,15 +16,6 @@ sh configure
 make
 cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
 cd ..
-
-wget https://github.com/NixOS/patchelf/archive/refs/tags/0.18.0.tar.gz
-tar -xf 0.18.0.tar.gz
-cd patchelf-0.18.0
-./bootstrap.sh
-./configure --prefix="$HOME/.local"
-make install
-strip --strip-unneeded ~/.local/bin/patchelf
-gzip -9 ~/.local/share/man/man1/patchelf.1
 
 patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
 patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
